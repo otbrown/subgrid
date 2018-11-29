480796c6509eaf96d25c23841209aa13108e024f
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2018-11-29 16:59:06 +0000

    pulse.c, makefile: initial commit, pure C implementation of the pulse test case, initialises and finalises

---
+++ examples/pulse.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "d3q15.h"
+
+int main (void) {
+  int nx = 10;
+  int ny = 10;
+  int nz = 50;
+  double tau = 0.5;
+  double amplitude = 0.01;
+  const int numel = (nx+2) * (ny+2) * (nz+2);
+  int i, j, k;
+
+  // Initialisation
+  printf("Creating lattice with nx = %d, ny = %d, nz = %d, and tau = %0.1f\n",
+          nx, ny, nz, tau);
+  Lattice * lat = d3q15_init(nx, ny, nz, tau, tau);
+  bc_pbc_init(lat);
+  lat->bc_func = bc_pbc_update;
+  force_none_init(lat);
+  lat->force_func = force_none_calc;
+
+  // Initialise density and velocity
+  for (i = 0; i < numel; i++) lat->rho_ptr[i] = 1.0;
+  for (i = 0; i < numel * DQ_d; i++) lat->u_ptr[i] = 0.0;
+
+  // Set enhanced density
+  for (i = 1; i <= nx; i++) {
+    for (j = 1; j <= ny; j++) {
+      DQ_rho_get(lat, i, j, nz/2) += amplitude;
+    }
+  }
+  double z_mean = 0.0;
+  for (k = 1; k <= nz; k++) {
+    z_mean += DQ_rho_get(lat, nx/2, ny/2, k);
+  }
+  z_mean /= nz;
+
+  // renormalise
+  for (i = 1; i <= nx; i++) {
+    for (j = 1; j <= ny; j++) {
+      for (k = 1; k <= nz; k++) {
+        DQ_rho_get(lat, i, j, k) /= z_mean;
+      }
+    }
+  }
+
+  // initFromHydroVars()
+  for (i = 1; i <= nx; i++) {
+    for (j = 1; j <= ny; j++) {
+      for (k = 1; k <= nz; k++) {
+        calc_equil(DQ_rho_get(lat,i,j,k), &DQ_u_get(lat,i,j,k,0), &DQ_f_get(lat,i,j,k,0));
+      }
+    }
+  }
+
+  // Finalisation
+  printf("Cleaning up.\n");
+  d3q15_destroy(lat);
+
+  return 0;
+}
